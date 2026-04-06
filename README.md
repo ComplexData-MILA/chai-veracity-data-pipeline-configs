0f79e0aab5191e351be3a3ae5bddcea8aa6ba593
jacobthebanana <jacob@banana.abay.cf>
2026-04-06 09:55:05 -0400

    Added feasibility annotation logic.
    Signed-off-by: jacobthebanana <jacob@banana.abay.cf>

--- README.md
+++ README.md
@@ -10,3 +10,42 @@ scripts/
     # Scripts for annotating dataset
     - annotation
 ```
+
+## Getting Started
+
+Store secrets in `.env`, based on `example.env`. 
+
+To update to a more recent version of the s3-data-tool package, run:
+
+```bash
+uv lock --upgrade-package s3-data-tool
+uv sync
+```
+
+For LLM-powered annotation, make sure the `sglang` server is installed in a virtual environment at `$SCRATCH/uv-venv/sglang/bin/activate`.
+
+## Retrieve, Annotate, Export
+
+Retrieve "base dataset"- these are initially stored as jsonl temp files in the S3.
+
+```bash
+uv run --env-file .env scripts/ingestion/bsky_trending.py
+```
+
+Merge these jsonl temp files of the base dataset into parquet.
+
+```bash
+uv run --env-file .env s3-data-tool-clean-up
+```
+
+Run annotation. 
+
+```bash
+# SLURM cluster and local SGLang instance
+sbatch slurm/filter_feasibility.slurm.sh
+
+# Alternatively, set OPENAI_BASE_URL and OPENAI_API_KEY
+uv run scripts/filter/feasibility.py \
+--model_name $MODEL_NAME \
+--max_concurrency 36
+```
--- scripts/filter/feasibility.py
+++ scripts/filter/feasibility.py
@@ -1,28 +1,72 @@
+import argparse
 import asyncio
 
+import backoff
+import openai
 from s3_data_tool import Annotation, DataItem, S3DataTool
 
-streaming_configs = S3DataTool.StreamingConfigs(chunk_size=10)
+with open("scripts/filter/template.txt") as template_file:
+    TEMPLATE = template_file.read()
 
+@backoff.on_exception(backoff.expo, [openai.APIConnectionError])
+async def _generate(
+    item: DataItem, model_name: str, oai_client: openai.AsyncOpenAI
+) -> Annotation:
+    """Annotate using LLM and template.
 
-async def annotate(item: DataItem) -> Annotation:
-    return Annotation(
-        data={"is_feasible": True, "explanation": f"Source: {item.data["text"]}"}
+    This function is designed to raise exceptions eagerly.
+    """
+    prompt = TEMPLATE.format(text=item.data["text"])
+    response = await oai_client.chat.completions.create(
+        model=model_name,
+        messages=[{"role": "user", "content": prompt}],
     )
+    output = response.choices[0].message.content
+    assert output is not None
+
+    *_explanations, _verdict = output.split("|")
+    explanation = "\n".join(_explanations)
+    verdict_int = int(_verdict)
+
+    return Annotation(data={"is_feasible": verdict_int, "explanation": explanation})
+
+
+async def annotate(
+    item: DataItem, model_name: str, oai_client: openai.AsyncOpenAI, max_retries: int
+) -> Annotation:
+    """Annotate- retry if inner generation loop does not work."""
+    exceptions = []
+    for _ in range(max_retries):
+        try:
+            return await _generate(item, model_name=model_name, oai_client=oai_client)
+        except Exception as e:
+            exceptions.append(e)
+
+    raise RuntimeError(exceptions)
 
 
 async def main():
-    # If another annotation worker with the same annotator_name is active and not expired,
-    # the following should raise an Error.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", required=True)
+    parser.add_argument("--max_concurrency", type=int, default=128)
+    parser.add_argument("--max_retries", type=int, default=6)
+    args = parser.parse_args()
+    oai_client = openai.AsyncOpenAI()
+
     async with S3DataTool().filter_for_annotation(
         name="posts",
-        annotator_name="custom_search",
+        annotator_name="feasibility_001",
         base_columns=["text"],
     ) as annotator_view:
         await annotator_view.annotate(
-            annotate,
-            max_concurrency=16,  # concurrency limits for the "annotate" function
-            streaming_configs=streaming_configs,
+            lambda item: annotate(
+                item,
+                model_name=args.model_name,
+                oai_client=oai_client,
+                max_retries=args.max_retries,
+            ),
+            max_concurrency=args.max_concurrency,
+            streaming_configs=S3DataTool.StreamingConfigs(chunk_size=10),
         )
 
 
